1c27e338bace717848aba4323c143590abfe99ee
Coen Jacobs <coenjacobs@gmail.com>
2012-11-17 22:16:09 +0100

    Docs update and added deprecated functions for ref
    Added only two functions with deprecated call to might be used outside
    the plugin by developers.

--- latest-update-date.php
+++ latest-update-date.php
@@ -2,25 +2,37 @@
 
 /*
 Plugin Name: Latest Update Date
-Version: 0.3
-Plugin URI: http://coenjacobs.net/wordpress/plugins/latest-update-date
-Description: Will show the date that the site has been update for the last time (in the footer and optionally at posts/pages)
+Version: 1.0-beta1
+Description: Will show the date that the site has been updates for the last time (in the footer and optionally in posts/pages via shortcode).
 Author: Coen Jacobs
-Author URI: http://coenjacobs.net/
+Author URI: http://coenjacobs.me/
 */
 
+// We don't need this in backend, so we only make a new instance of this class on frontend
 if ( ! is_admin() ) {
 	global $cj_latest_update_date;
 	$cj_latest_update_date = new CJ_Latest_Update_Date();
 }
 
+/**
+ * Main plugin class
+ *
+ * @since 1.0
+ */
 class CJ_Latest_Update_Date {
-	private $date_format;
-	private $before_element;
-	private $after_element;
-	private $before_text;
-	private $after_text;
+	private $date_format;		// Format of the date to display
+	private $before_element;	// Element shown before the date (and text)
+	private $after_element;		// Element shown after the date (and text)
+	private $before_text;		// Text shown before the date
+	private $after_text;		// Text shown after the date
 
+	/**
+	 * Constructor: Set default markup variables that can be changed through filters.
+	 * Sets up shortcode for use in posts/pages and add the filter for the footer output.
+	 * 
+	 * @return void
+	 * @since 1.0
+	 */
 	public function __construct() {
 		$this->date_format = apply_filters( 'latest_update_date_date_format', get_option( 'date_format' ) );
 		$this->before_element = apply_filters( 'latest_update_date_before_element', '<p>' );
@@ -32,12 +44,28 @@ class CJ_Latest_Update_Date {
 		add_filter( 'wp_footer', array( &$this, 'output_footer' ) );
 	}
 
+	/**
+	 * Returns the post that has the latest updated date
+	 *
+	 * @access private
+	 * @return post object or false if no post found
+	 * @since 1.0
+	 */
 	private function get_latest_updated_post() {
 		$posts = get_posts( apply_filters( 'latest_update_date_query_args', array( 'post_type' => array( 'post', 'page' ), 'numberposts' => 1, 'orderby' => 'modified' ) ) );
 
+		// Check if this query has at least one result and return that, or false if no results
 		return ( isset( $posts[0] ) ) ? $posts[0] : false;
 	}
 
+	/**
+	 * Returns the output of the shortcode use
+	 *
+	 * @access public
+	 * @param $atts array Parameters for shortcode to override defaults
+	 * @return string output of the shortcode
+	 * @since 1.0
+	 */
 	public function get_shortcode( $atts ) {
 		extract( shortcode_atts( array(
 			'date_format'    => $this->date_format,
@@ -48,6 +76,7 @@ class CJ_Latest_Update_Date {
 			'post_id'        => false,
 		), $atts ) );
 
+		// If post_id specified, we will use that post_id, or else get via $this->get_latest_updated_post()
 		if ( ! $post_id ) {
 			if ( ! $latest_updated_post = $this->get_latest_updated_post() ) {
 				return;
@@ -56,6 +85,7 @@ class CJ_Latest_Update_Date {
 			$latest_updated_post = get_post( absint( $post_id ) );
 		}
 
+		// Final check if we have a post, create and return the output if so
 		if ( $latest_updated_post ) {
 			$output = $before_element;
 			$output .= $before_text;
@@ -67,6 +97,13 @@ class CJ_Latest_Update_Date {
 		}
 	}
 
+	/**
+	 * Echos the output in the footer
+	 *
+	 * @access public
+	 * @return void
+	 * @since 1.0
+	 */
 	public function output_footer() {
 		if ( apply_filters( 'latest_update_date_show_in_footer', true ) ) {
 			if ( $latest_updated_post = $this->get_latest_updated_post() ) {
@@ -80,6 +117,24 @@ class CJ_Latest_Update_Date {
 			}
 		}
 	}
+}
+
+/**
+ * Old function called to get latest update date
+ *
+ * @deprecated 1.0
+ */
+function get_latest_update() {
+	_deprecated_function( __FUNCTION__, '1.0' );
+}
+
+/**
+ * Old function called to show the latest date in the footer
+ *
+ * @deprecated 1.0
+ */
+function latest_update_date_footer() {
+	_deprecated_function( __FUNCTION__, '1.0' );
 }
 
 ?>
