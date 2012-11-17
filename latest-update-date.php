7eb2152d2e69929c268178492888e0577d7d85b6
Coen Jacobs <coenjacobs@gmail.com>
2012-11-17 20:54:03 +0100

    Moved functions to class methods for namespacing

--- latest-update-date.php
+++ latest-update-date.php
@@ -9,26 +9,35 @@ Author: Coen Jacobs
 Author URI: http://coenjacobs.net/
 */
 
-function get_latest_update() {
-	$query = mysql_query("SELECT MAX(post_modified) AS maxnum FROM wp_posts WHERE post_status = 'publish'");
-	$row = mysql_fetch_array($query);
-	return $row['maxnum'];
+if ( ! is_admin() ) {
+	global $cj_latest_update_date;
+	$cj_latest_update_date = new CJ_Latest_Update_Date();
 }
 
-function latest_update_date_footer() {
-	$conditionals = get_option('latest-update_conditionals');
-	
-	if ( apply_filters( 'latest_update_date_show_in_footer', true ) ) {
-		$output = apply_filters( 'latest_update_date_before_element', '<p>' );
-		$output .= apply_filters( 'latest_update_date_before_text', 'Latest update date:' );
-		$output .= ' ' . mysql2date( 'j-n-Y', get_latest_update() ) . ' ';
-		$output .= apply_filters( 'latest_update_date_after_text', '' );
-		$output .= apply_filters( 'latest_update_date_after_element', '</p>' );
+class CJ_Latest_Update_Date {
+	public function __construct() {
+		add_filter( 'wp_footer', array( &$this, 'output_footer' ) );
+	}
+
+	private function get_latest_update() {
+		$query = mysql_query("SELECT MAX(post_modified) AS maxnum FROM wp_posts WHERE post_status = 'publish'");
+		$row = mysql_fetch_array($query);
+		return $row['maxnum'];
+	}
+
+	public function output_footer() {
+		$conditionals = get_option('latest-update_conditionals');
 		
-		echo $output;
+		if ( apply_filters( 'latest_update_date_show_in_footer', true ) ) {
+			$output = apply_filters( 'latest_update_date_before_element', '<p>' );
+			$output .= apply_filters( 'latest_update_date_before_text', 'Latest update date:' );
+			$output .= ' ' . mysql2date( 'j-n-Y', $this->get_latest_update() ) . ' ';
+			$output .= apply_filters( 'latest_update_date_after_text', '' );
+			$output .= apply_filters( 'latest_update_date_after_element', '</p>' );
+			
+			echo $output;
+		}
 	}
 }
-
-add_filter( 'wp_footer', 'latest_update_date_footer' );
 
 ?>
